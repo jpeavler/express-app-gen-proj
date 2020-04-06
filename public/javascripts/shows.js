e4dbd80298d86ca9d39c19ec5c0150b5d5f8ab33
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-06 16:51:55 -0600

    add form to post new Show

--- public/javascripts/shows.js
+++ public/javascripts/shows.js
@@ -1,6 +1,26 @@
 console.log('Hello from shows.js');
 const deleteButtons = document.getElementsByClassName('delete');
+const titleInput = document.getElementById('title');
+const seasonsInput = document.getElementById('numSeasons');
+const descInput = document.getElementById('desc');
+const submit = document.getElementById('submit');
 
+
+const submitHandler = (event) => {
+    console.log("Attempt to submit");
+    let subData = {
+        'title': titleInput.value,
+        'numSeasons' : seasonsInput.value,
+        'desc' : descInput.value
+    }
+    fetch(`/api/shows/`, {
+        method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(subData)
+    }).then(() => window.location.replace('/shows'));
+}
 const deleteHandler = (event) => {
     console.log("ID: ", event.target.id);
     fetch(`/api/shows/${event.target.id}`, {
@@ -11,6 +31,7 @@ const deleteHandler = (event) => {
 for(let i = 0; i < deleteButtons.length; i++){
     deleteButtons[i].addEventListener('click', deleteHandler);
 }
+submit.addEventListener('click', submitHandler);
 
 
 fetch('/api/shows')
